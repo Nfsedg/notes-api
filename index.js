e1e0f2733a4eda2f09cdb137f58c2da79a9193cf
Edgar Pech <edgaropech@hotmail.com>
2022-02-21 10:27:02 -0500

    add heroku needed config

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/api/users', usersRouter);
 app.use(notFound);
 app.use(handleErrors);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3006;
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
 });
